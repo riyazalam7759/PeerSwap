3ae988cad9b4eccc68136c3f4ea4866f9b97b47b
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-02-28 16:15:06 +0530

    just checking data is going or not going to mongodb from login page

--- PeerSwap.js
+++ PeerSwap.js
@@ -1,11 +1,23 @@
 import express from 'express'
-
+import { MongoClient } from 'mongodb'
 import path from 'path'
 
 const app = express();
+
+//for mongodb connection
+const dbName ="MiniProjectDB";
+const collectionName="loginCollection";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const connection = async()=>{
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
+
+//middleware
+app.use(express.urlencoded({extended:true}))
 const publicPath=path.resolve('public')
 app.use(express.static(publicPath))
-
 app.set("view engine" ,'ejs')
 
 app.get("/",(req , resp)=>{
@@ -44,8 +56,13 @@ app.get("/chatSection",(req,resp)=>{
     resp.render("chatSection")
 })
 
-app.post("/login",(req , resp)=>{
+app.post("/login",async (req , resp)=>{
+    const db= await connection();
+    const collection =db.collection(collectionName);
+    const result = await collection.insertOne(req.body);
+    if(result)
     resp.redirect("/home")
+     else resp.redirect("/login")
 })
 
 
